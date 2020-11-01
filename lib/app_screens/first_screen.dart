f9e4fb79e82c23e19ea9cd4cc1780520ae12e6fc
Arif Shahriar <arifshahriar028@gmail.com>
2020-11-01 21:18:32 +0600

    Practise: 1

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 
-class FirstScreen extends StatelessWidget{
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Material(
       color: Colors.blueAccent,
       child: Center(
-          child: Text(
-            "Hello flutter devs",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 40),
-          )
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20.0),
+          color: Colors.amber,
+          margin: EdgeInsets.all(15.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      "Welcome!",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontSize: 15.0),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Hello flutter devs",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontSize: 20.0),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      "Welcome!",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontSize: 15.0),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Hello flutter devs",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontSize: 20.0),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
