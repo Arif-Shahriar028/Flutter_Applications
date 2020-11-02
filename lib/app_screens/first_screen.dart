1b9fb7792450916185970a1b48977f5e22e20c21
Arif Shahriar <arifshahriar028@gmail.com>
2020-11-02 10:31:54 +0600

    Practise: 2

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -62,10 +62,66 @@ class FirstScreen extends StatelessWidget {
                   )
                 ],
               ),
+              ManufactureImageAsset(),
+              ButtonClass()
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ManufactureImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    AssetImage assetImage = AssetImage('images/manufacture.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+
+class ButtonClass extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.only(top: 20.0),
+      width: 250,
+      height: 50,
+      child: RaisedButton(
+        color: Colors.deepOrangeAccent,
+        child: Text(
+          "Click button",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            decoration: TextDecoration.none, // remove underline
+            fontSize: 30.0,
+            color: Colors.white
+          ),
+        ),
+        elevation: 6.0,
+        onPressed: (){
+          buttonAction(context);
+        },
+      ),
+    );
+  }
+
+  void buttonAction(BuildContext context){
+    var alertDialog = AlertDialog(
+      title: Text("Button Pressed"),
+      content: Text("bbutton is pressed successfully"),
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext  context){
+        return alertDialog;
+      }
     );
   }
 }
